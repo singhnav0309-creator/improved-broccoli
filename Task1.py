d0b6321e0fa1f4ad82dbbe67bc0174134453de78
Navi <singhnav0309@gmail.com>
2026-02-25 14:11:07 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -10,7 +10,7 @@ print(f"Factorial of {num} is: {fact_rec(num)}")
 
 
 """ We take a number as an arguement for fact_rec function
-if number is equal to 1,
+if number is equal to 1, it returns 1
 otherwise, it returns factorial of the number.
 return None """
 
