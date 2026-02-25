972286c54f7fc68e895de9ec1a79ecdfe18f99a7
singh0309 <singhnav0309@gmail.com>
2026-02-25 14:02:19 +0530

    commit message

---
+++ Task2.py
@@ -0,0 +1,12 @@
+import math
+
+num = int(input("Enter a number: "))
+output = math.sqrt(num)
+print(f"square root: {output}")
+
+output = math.log(num)
+print(f"Logarithm: {output}")
+
+output = math.sin(num)
+print(f"Sine: {output}")
+
